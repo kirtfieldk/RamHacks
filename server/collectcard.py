2d67255dd761620f20081d8a78dd4728eb99c3c8
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-10-15 13:38:42 -0400

    made methods and clean up

--- server/collectcard.py
+++ server/collectcard.py
@@ -16,31 +16,33 @@ class CreditCard:
 
 
 # URLs to scrape through
-url = [
+chaseUrl = [
 "https://creditcards.chase.com/cash-back-credit-cards/chase-freedom-unlimited?CELL=6TKX&jp_aid=cc/mptarg1/int/FREU/ccrew1",
-"https://creditcards.chase.com/rewards-credit-cards/chase-sapphire-preferred?CELL=6TKX&jp_aid=cc/mptarg1/int/SAPP/ccrew2",
-"https://creditcards.chase.com/travel-credit-cards/marriott-bonvoy-boundless?CELL=6TKX&jp_aid=cc/mptarg1/int/MARC/cctrav1",
-"https://creditcards.chase.com/cash-back-credit-cards/chase-freedom?CELL=6TKX",
-"https://creditcards.chase.com/rewards-credit-cards/chase-sapphire-reserve?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/southwest-priority?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/southwest-plus?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/southwest-premier?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/united-explorer?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/united-travelbank?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/united-mileageplus-club?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/british-airways?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/aer-lingus?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/marriott-bonvoy-bold?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/world-of-hyatt-credit-card?CELL=6TKX",
-"https://creditcards.chase.com/rewards-credit-cards/disney-premier?CELL=6TKX"
-"https://creditcards.chase.com/rewards-credit-cards/disney-rewards?CELL=6TKX",
-"https://creditcards.chase.com/travel-credit-cards/ihg-rewards-club-premier?CELL=6TKX",
-"https://creditcards.chase.com/rewards-credit-cards/starbucks-rewards?CELL=6TKX",
-"https://creditcards.chase.com/cash-back-credit-cards/amazon-rewards?CELL=6TKX",
-"https://creditcards.chase.com/small-business-credit-cards/ink-unlimited?CELL=6TKX",
-"https://creditcards.chase.com/small-business-credit-cards/ink-cash?CELL=6TKX",
-"https://creditcards.chase.com/small-business-credit-cards/ink-business-preferred?CELL=6TKX",
-# "https://www.americanexpress.com/us/credit-cards/card/gold-card/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-GoldCard",
+# "https://creditcards.chase.com/rewards-credit-cards/chase-sapphire-preferred?CELL=6TKX&jp_aid=cc/mptarg1/int/SAPP/ccrew2",
+# "https://creditcards.chase.com/travel-credit-cards/marriott-bonvoy-boundless?CELL=6TKX&jp_aid=cc/mptarg1/int/MARC/cctrav1",
+# "https://creditcards.chase.com/cash-back-credit-cards/chase-freedom?CELL=6TKX",
+# "https://creditcards.chase.com/rewards-credit-cards/chase-sapphire-reserve?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/southwest-priority?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/southwest-plus?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/southwest-premier?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/united-explorer?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/united-travelbank?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/united-mileageplus-club?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/british-airways?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/aer-lingus?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/marriott-bonvoy-bold?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/world-of-hyatt-credit-card?CELL=6TKX",
+# "https://creditcards.chase.com/rewards-credit-cards/disney-premier?CELL=6TKX"
+# "https://creditcards.chase.com/rewards-credit-cards/disney-rewards?CELL=6TKX",
+# "https://creditcards.chase.com/travel-credit-cards/ihg-rewards-club-premier?CELL=6TKX",
+# "https://creditcards.chase.com/rewards-credit-cards/starbucks-rewards?CELL=6TKX",
+# "https://creditcards.chase.com/cash-back-credit-cards/amazon-rewards?CELL=6TKX",
+# "https://creditcards.chase.com/small-business-credit-cards/ink-unlimited?CELL=6TKX",
+# "https://creditcards.chase.com/small-business-credit-cards/ink-cash?CELL=6TKX",
+# "https://creditcards.chase.com/small-business-credit-cards/ink-business-preferred?CELL=6TKX"
+]
+aeCards = [
+"https://www.americanexpress.com/us/credit-cards/card/gold-card/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-GoldCard",
 # "https://www.americanexpress.com/us/credit-cards/card/platinum-delta-skymiles/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-DeltaPlatinum",
 # "https://www.americanexpress.com/us/credit-cards/card/amex-everyday/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-AED",
 # "https://www.americanexpress.com/us/credit-cards/card/platinum/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-Platinum",
@@ -55,8 +57,10 @@ url = [
 # "https://www.americanexpress.com/us/credit-cards/card/hilton-honors/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-HiltonHonors",
 # "https://www.americanexpress.com/us/credit-cards/card/delta-blue/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-DeltaBlue",
 # "https://www.americanexpress.com/us/credit-cards/card/delta-reserve/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-DeltaReserve",
-# "https://www.americanexpress.com/us/credit-cards/card/blue/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-Blue",
-# "https://www.capitalone.com/credit-cards/venture/",
+# "https://www.americanexpress.com/us/credit-cards/card/blue/?eep=25330&linknav=US-Acq-Shop-Consumer-VAC-Prospect-ViewCardDetail-Blue"
+]
+coCard = [
+"https://www.capitalone.com/credit-cards/venture/",
 # "https://www.capitalone.com/credit-cards/ventureone/",
 # "https://www.capitalone.com/credit-cards/savor-dining-rewards/",
 # "https://www.capitalone.com/credit-cards/savorone-dining-rewards/"
@@ -89,18 +93,25 @@ crsr = connection.cursor()
 # VALUES (NULL, "Marriott", 22, 44);"""
 # crsr.execute(command)
 
+def webScrap(urls):
+    for data in urls:
+        response = requests.get(data)
+        page = urlopen(data)
+        soup = BeautifulSoup(page, "html.parser")
+        if urls == chaseUrl:
+            parseChase(soup)
+        if urls == aeCards:
+            parseAe(soup)
+        if urls == coCard:
+            parseCoCard(soup)
 
 
-
-for data in url:
+def parseChase(soup):
     # New arrayeach time
-    response = requests.get(data)
-    page = urlopen(data)
-    soup = BeautifulSoup(page, "html.parser")
-    
     for element in soup.find_all("sup"):
         element.decompose()
     text = soup.get_text()
+    # Grabs the title of the credit cards
     titlebox = soup.find("h1", attrs = {'class': "card-title"})
     # benefitsbox = soup.find("div", attrs = {'class', "primary-item"})
     title = titlebox.text.strip()
@@ -112,19 +123,55 @@ for data in url:
     crsr.execute(command)
     connection.commit()
 
-crsr.execute("SELECT * FROM creditcards") 
-print("fetchall:")
-result = crsr.fetchall() 
-for r in result:
-    print(r)
+def parseAe(soup):
+    text = soup.get_text()
+    for element in soup.find("span"):
+        print(element)
+    # Grabs the title of the credit cards
+    titlebox = soup.find("span", attrs = {'data-qe-id': "card_title"})
+    # benefitsbox = soup.find("div", attrs = {'class', "primary-item"})
+    # title = titlebox.text.strip()
+    # print (titlebox)
+    # # benefits = benefitsbox.text.strip()
+    # command = """INSERT INTO creditcards(id, name, credit_low, credit_high)
+    # VALUES (NULL, '{title}', 22, 44);"""
+    # command = command.format(title=title)
+    # crsr.execute(command)
+    # connection.commit()
 
 
-connection.close()
+def parseCoCard(soup):
+    text = soup.get_text()
+    titlebox = soup.find("h1", attrs = {'class': 'non-mobile'})
+    title = titlebox.text.strip()
+    print(title)
+    command = """INSERT INTO creditcards(id, name, credit_low, credit_high)
+    VALUES (NULL, '{title}', 22, 44);"""
+    command = command.format(title=title)
+    crsr.execute(command)
+    connection.commit()
 
-    # TODO STORE IN SQL DATABASE
+
+def printDb():
+    crsr.execute("SELECT * FROM creditcards") 
+    print("fetchall:")
+    result = crsr.fetchall() 
+    for r in result:
+        print(r)
 
 
 
+
+
+
+def main():
+    webScrap(coCard);
+    printDb()
+    connection.close()
+
+
+if __name__ == '__main__':
+    main()
 
 
 
