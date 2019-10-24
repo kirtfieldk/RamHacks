17bfb5499f5445ef4d8b723afd0ac5510cdc2899
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-10-24 13:08:45 -0400

    Revamp a bonvoy dup

---
+++ server/routes/consumerRoute.py
@@ -0,0 +1,10 @@
+
+# TODO fetch all consumer
+
+
+def get_consumer():
+    # TODO connect to SQL
+    # TODO check for errors
+    # TODO return list of consumers
+
+    # TODO Create middleware for quering
